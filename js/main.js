e89af3366dd00e27ba5c9a2b73363251a73e6e40
Cori Fowler <fowler.cori@gmail.com>
2015-10-08 12:05:41 -0400

    Used jQuery and underscore to get items to display on the page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 (function () {
 
-  console.log('It Works!');
+var templateString = $('#itemTemplate').text();
+console.log(templateString);
+
+var renderTemplate = _.template(templateString);
+
+// bring up items I want
+
+
+_.each( etsy.results, function(item){
+  var itemInfo = renderTemplate(item);
+  $('.results-area').append(itemInfo);
+});
+
 
 }());
